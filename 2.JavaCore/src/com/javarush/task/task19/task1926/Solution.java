ecd49bfc3f60a003e61581cb9f6f522ea19e88d7
Anatolii Gusakovskii <gusakovskii.anatolii@gmail.com>
2020-08-03 08:43:09 +0300

    gusakovskyLocal : deleted temp code in 1926

--- 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -14,16 +14,12 @@ public class Solution {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             fileName = reader.readLine();
             try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
-           while ((line = fileReader.readLine()) != null) {
+                while ((line = fileReader.readLine()) != null) {
                     for (int i = line.length() - 1; i >= 0; i--)
                         System.out.print(line.charAt(i));
-               System.out.println();
+                    System.out.println();
                 }
-//            while ((line = fileReader.readLine()) != null) {
-//                System.out.println(new StringBuilder(line).reverse());
-//            }
-            }
-            catch(FileNotFoundException sad) {
+            } catch (FileNotFoundException sad) {
                 System.out.println("FileNotFoundException catched");
             }
         } catch (IOException b) {
