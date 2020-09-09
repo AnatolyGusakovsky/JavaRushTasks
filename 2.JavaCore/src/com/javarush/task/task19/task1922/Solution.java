c0150dddbb2f24321236f1adf77d296aa28407d5
Anatoly <gusakovsky.anatoly@gmail.com>
2020-09-09 08:52:12 +0300

    Revert "gusakovskyLocal : 1921,1922,1923 - done"

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -1,13 +1,7 @@
 package com.javarush.task.task19.task1922;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /* 
 Ищем нужные строки
@@ -15,33 +9,14 @@ import java.util.regex.Pattern;
 
 public class Solution {
     public static List<String> words = new ArrayList<String>();
-
+// try to except static block
     static {
         words.add("файл");
         words.add("вид");
         words.add("В");
     }
+
     public static void main(String[] args) {
-try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-    String fileName = reader.readLine();
-    try(FileReader fileReader = new FileReader(fileName);
-    BufferedReader bufferedReader = new BufferedReader(fileReader)){
-        String line;
-        while((line = bufferedReader.readLine()) != null) {
-          int count = 0;
-          for(String word : words) {
-              Pattern pattern = Pattern.compile("\\b" + word + "\\b");
-              Matcher matcher = pattern.matcher(line);
-               while(matcher.find())
-                  count = count + 1;
-          }
-          if(count == 2)
-              System.out.println(line);
-        }
-    }
-}
- catch (IOException e) {
-    e.printStackTrace();
- }
+
     }
 }
--- 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -1,32 +1,10 @@
 package com.javarush.task.task19.task1923;
 
-import java.io.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /* 
 Слова с цифрами
 */
 
 public class Solution {
     public static void main(String[] args) {
-        String sourceFile, fileToWrite;
-        sourceFile = args[0];
-        fileToWrite = args[1];
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(sourceFile));
-        FileWriter fileWriter = new FileWriter(fileToWrite)) {
-            String line;
-            while((line = bufferedReader.readLine()) != null) {
-                Pattern pattern = Pattern.compile("\\b\\S+\\d+\\S*\\b");
-                Matcher matcher = pattern.matcher(line);
-                while (matcher.find()) {
-                    String founded = line.substring(matcher.start(), matcher.end());
-                    fileWriter.write(founded + " ");
-                }
-            }
-        }
-        catch (IOException exception) {
-            exception.printStackTrace();
-        }
     }
 }
