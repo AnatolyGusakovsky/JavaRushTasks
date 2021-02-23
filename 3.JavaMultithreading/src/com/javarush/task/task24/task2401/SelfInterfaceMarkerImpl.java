6cbb5b70560cd45212564bc6e2988fd340ce8006
dmitryreznik <dmitry.reznik@compass.com>
2021-02-23 20:24:11 +0200

    Some tasks done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+
+    public void method1() {
+
+    }
+
+
+    public void method2() {
+
+    }
+
+    public void method3(){}
+    public void method4(){}
+}
